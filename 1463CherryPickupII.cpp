3bfc8689770901afcdff174c20a5d9925af59c45
VaibhavDhaygonde7 <71217777+VaibhavDhaygonde7@users.noreply.github.com>
2025-09-08 20:13:21 +0530

    Update 1463CherryPickupII.cpp

--- 1463CherryPickupII.cpp
+++ 1463CherryPickupII.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 using namespace std; 
 
-// recursive solution 
 class Solution {
 public:
     int cherryPickup(vector<vector<int>>& grid) {
@@ -41,3 +40,206 @@ public:
 
     }
 };
+
+
+class Solution2 {
+public:
+    int cherryPickup(vector<vector<int>>& grid) {
+        vector<vector<vector<int>>> dp(grid.size()); 
+        vector<int> one_d(grid[0].size(), -1); 
+        vector<vector<int>> two_d(grid[0].size()); 
+
+        for(int i = 0; i < grid[0].size(); i++){
+            two_d[i] = one_d; 
+        }
+
+        for(int i = 0; i < grid.size(); i++){
+            dp[i] = two_d; 
+        }
+
+        secondLastRow(grid, dp); 
+
+        // for(int i = 0; i < grid[0].size(); i++){
+        //     for(int j = i+1; j < grid[0].size(); j++){
+        //         cout << "[" << i << ", " << j << "] = " << dp[grid.size()-2][i][j] << endl; 
+        //     }
+        // }
+
+        // updateRow(grid, dp, 1); 
+
+        // for(int i = 0; i < grid[0].size(); i++){
+        //     for(int j = i+1; j < grid[0].size(); j++){
+        //         cout << "[" << i << ", " << j << "] = " << dp[1][i][j] << endl; 
+        //     }
+        // }
+
+        for(int i = grid.size() - 3; i >= 0; i--){
+            updateRow(grid, dp, i); 
+        }
+
+        return dp[0][0][grid[0].size()-1]; 
+
+    }
+
+    void updateRow(vector<vector<int>>& grid, vector<vector<vector<int>>>& dp, int ind){
+        vector<int> directions = {-1,0,1}; 
+
+        for(int i = 0; i < grid[0].size(); i++){
+            for(int j = i+1; j < grid[0].size(); j++){
+                for(int n1 : directions){
+                    if(i + n1 >= 0 && i + n1 < grid[0].size()){
+                        for(int n2 : directions){
+                            if(j + n2 >= 0 && j + n2 < grid[0].size() && i + n1 < j + n2){
+                                dp[ind][i][j] = max(dp[ind][i][j], grid[ind][i] + grid[ind][j] + dp[ind+1][i+n1][j+n2]); 
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    void secondLastRow(vector<vector<int>>& grid, vector<vector<vector<int>>>& dp){
+        vector<int> directions = {-1, 0, 1}; 
+
+        int x = grid.size() - 2; 
+
+        for(int i = 0; i < grid[0].size(); i++){
+            for(int j = i+1; j < grid[0].size(); j++){
+                for(int n1 : directions){
+                    if(i + n1 >= 0 && i + n1 < grid[0].size()){
+                        for(int n2: directions){
+                            if(j + n2 >= 0 && j + n2 < grid[0].size() && i + n1 < j + n2){
+                                dp[x][i][j] = max(dp[x][i][j], grid[x][i] + grid[x][j] + grid[x+1][i+n1] + grid[x+1][j+n2]); 
+                                // cout << grid[x][i] << " " << grid[x][j] << " " << grid[x+1][i+n1] << " " << grid[x+1][j+n2] << endl; 
+                                // cout << "x: " << x << ", i: " << i << ", j: " << j << ", dp[x][i][j] = " << dp[x][i][j] << endl; 
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+};
+
+
+
+// optimizing space 
+class Solution3 {
+public:
+    int cherryPickup(vector<vector<int>>& grid) {
+        vector<vector<vector<int>>> dp(grid.size()); 
+        vector<int> one_d(grid[0].size(), -1); 
+        vector<vector<int>> next(grid[0].size()); 
+        vector<vector<int>> curr(grid[0].size()); 
+
+        for(int i = 0; i < grid[0].size(); i++){
+            next[i] = one_d; 
+            curr[i] = one_d; 
+        }
+
+        secondLastRow(grid, next); 
+
+        // for(int i = 0; i < grid[0].size(); i++){
+        //     for(int j = i+1; j < grid[0].size(); j++){
+        //         cout << "[" << i << ", " << j << "] = " << next[i][j] << endl; 
+        //     }
+        // }
+
+        // updateRow(grid, curr, next, 1); 
+    
+
+        
+        // for(int i = 0; i < grid[0].size(); i++){
+        //     for(int j = i+1; j < grid[0].size(); j++){
+        //         cout << "[" << i << ", " << j << "] = " << curr[i][j] << endl; 
+        //     }
+        // }
+
+        // updateRow(grid, curr, next, 0); 
+    
+
+        
+        // for(int i = 0; i < grid[0].size(); i++){
+        //     for(int j = i+1; j < grid[0].size(); j++){
+        //         cout << "[" << i << ", " << j << "] = " << curr[i][j] << endl; 
+        //     }
+        // }
+
+
+        for(int i = grid.size() - 3; i >= 0; i--){
+            updateRow(grid, curr, next, i); 
+        }
+
+        return next[0][grid[0].size()-1]; 
+
+    }
+
+    void updateRow(vector<vector<int>>& grid, vector<vector<int>>& curr, vector<vector<int>>& next, int ind){
+        vector<int> directions = {-1,0,1}; 
+
+        for(int i = 0; i < grid[0].size(); i++){
+            for(int j = i+1; j < grid[0].size(); j++){
+                for(int n1 : directions){
+                    if(i + n1 >= 0 && i + n1 < grid[0].size()){
+                        for(int n2 : directions){
+                            if(j + n2 >= 0 && j + n2 < grid[0].size() && i + n1 < j + n2){
+                                curr[i][j] = max(curr[i][j], grid[ind][i] + grid[ind][j] + next[i+n1][j+n2]); 
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        for(int i = 0; i < grid[0].size(); i++){
+            for(int j = i+1; j < grid[0].size(); j++){
+                next[i][j] = curr[i][j]; 
+            }
+        }
+
+
+
+    }
+
+    void secondLastRow(vector<vector<int>>& grid, vector<vector<int>>& dp){
+        vector<int> directions = {-1, 0, 1}; 
+
+        int x = grid.size() - 2; 
+
+        for(int i = 0; i < grid[0].size(); i++){
+            for(int j = i+1; j < grid[0].size(); j++){
+                for(int n1 : directions){
+                    if(i + n1 >= 0 && i + n1 < grid[0].size()){
+                        for(int n2: directions){
+                            if(j + n2 >= 0 && j + n2 < grid[0].size() && i + n1 < j + n2){
+                                dp[i][j] = max(dp[i][j], grid[x][i] + grid[x][j] + grid[x+1][i+n1] + grid[x+1][j+n2]); 
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+};
+
+
+
+int main(){
+
+    vector<vector<int>> grid = {
+        {3,1,1},
+        {2,5,1},
+        {1,5,5},
+        {2,1,1}
+    }; 
+
+    Solution3 s = Solution3(); 
+
+    cout << s.cherryPickup(grid) << endl; 
+
+    return 0;   
+}
